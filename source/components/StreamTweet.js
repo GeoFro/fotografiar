379343771ea84a2578b0227eaae1e0c90ed06a80
GeoFro <georgeharoldfrost@gmail.com>
2018-04-25 20:08:43 +1000

    Added Header and Tweet components

--- source/components/StreamTweet.js
+++ source/components/StreamTweet.js
@@ -30,6 +30,55 @@ class StreamTweet extends Component {
     window.fotografiar.tweetHtml = componentDOMRepresentation.children[1].outerHTML;
   }
 
+  componentWillReceiveProps(nextProps) {
+    console.log('[Fotografiar] StreamTweet: 4. Running componentWillReceiveProps()')
+
+    const { tweet: currentTweet } = this.props;
+    const { tweet: nextTweet } = nextProps;
+
+    const currentTweetLength = currentTweet.text.length;
+    const nextTweetLength = nextTweet.text.length;
+
+    const isNumberOfCharactersIncreasing = (nextTweetLength > currentTweetLength);
+    let headerText;
+
+    this.setState({
+      numberOfCharactersIsIncreasing: isNumberOfCharactersIncreasing
+    });
+
+    if (isNumberOfCharactersIncreasing) {
+      headerText = 'Number of characters is increasing';
+    } else {
+      headerText = 'Latest public photo from Twitter';
+    }
+
+    this.setState({
+      headerText
+    });
+
+    window.fotografiar.numberOfReceivedTweets++;
+  }
+
+  // If the next Tweet's text is longer than 1 character then it will be true //
+  // and then it will update. Otherwise false and it won't //
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Fotografiar] StreamTweet: 5 Running shouldComponentUpdate()')
+
+    return (nextProps.tweet.text.length > 1);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[Fotografiar] StreamTweet 6. Running componentWillUpdate()')
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('[Fotografiar] StreamTweet 7. Running componentDidUpdate()')
+
+    window.fotografiar.numberOfDisplayedTweets++;
+  }
+
+  // End of updating cycle
+
   componentWillUnMount() {
     console.log('[Fotografiar] StreamTweet: 8. Running componentWillUnMount()');
 
