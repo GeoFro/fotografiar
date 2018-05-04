5492deecda549b74e46773afb301851d3cabd7f4
GeoFro <georgeharoldfrost@gmail.com>
2018-05-04 10:22:27 +1000

    Updates

--- source/app.js
+++ source/app.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Application from './components/Application';
 
+import io from 'socket.io-client';
+const socket = io('http://localhost');
+
 ReactDOM.render(
   <Application />,
   document.getElementById('react-application')
