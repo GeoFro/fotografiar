43e9c8bad09937a96af01212e89b058d52f427bb
GeoFro <georgeharoldfrost@gmail.com>
2018-05-10 18:02:22 +1000

    Restructured Application component

--- source/components/Application.js
+++ source/components/Application.js
@@ -3,54 +3,22 @@ import Stream from './Stream';
 import Collection from './Collection';
 
 class Application extends Component {
-  state = {
-    collectionTweets: {}
-  }
-
-  addTweetToCollection = (tweet) => {
-    const { collectionTweets } = this.state;
-
-    collectionTweets[tweet.id] = tweet;
-
-    this.setState({
-      collectionTweets: collectionTweets
-    });
-  }
-
-  removeTweetFromCollection = (tweet) => {
-    const { collectionTweets } = this.state;
-
-    delete collectionTweets[tweet.id];
-
-    this.setState({
-      collectionTweets: collectionTweets
-    });
-  }
-
-  removeAllTweetsFromCollection = () => {
-    this.setState({
-      collectionTweets: {}
-    });
-  }
-
   render() {
     const {
-      addTweetToCollection,
-      removeTweetFromCollection,
-      removeAllTweetsFromCollection
-    } = this;
+      collectionTweets
+    } = this.state;
 
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-4 text-center">
-            <Stream onAddTweetToCollection={addTweetToCollection}/>
+            <Stream onAddTweetToCollection={this.addTweetToCollection}/>
           </div>
           <div className="col-md-8">
             <Collection
-              tweets={this.state.collectionTweets}
-              onRemoveTweetFromCollection={removeTweetFromCollection}
-              onRemoveAllTweetsFromCollection={removeAllTweetsFromCollection}
+              tweets={collectionTweets}
+              onRemoveTweetFromCollection={this.removeTweetFromCollection}
+              onRemoveAllTweetsFromCollection={this.removeAllTweetsFromCollection}
             />
           </div>
         </div>
