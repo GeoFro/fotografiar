80469670adb9c93b342c885dff6b5172d42c39a8
GeoFro <georgeharoldfrost@gmail.com>
2018-05-10 15:38:29 +1000

    Jest tests with enzyme

--- source/components/Header.test.js
+++ source/components/Header.test.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 import Header, { DEFAULT_HEADER_TEXT } from './Header';
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('Header', () => {
   test('renders default header text', () => {
-    const component = renderer.create(
+    const wrapper = shallow(
       <Header/>
     );
 
-    const tree = component.toJSON();
-    const firstChild = tree.children[0];
-
-    expect(firstChild).toBe(DEFAULT_HEADER_TEXT);
+    expect(wrapper.find('h2')).toHaveLength(1);
+    expect(wrapper.contains(DEFAULT_HEADER_TEXT)).toBe(true);
   });
 
   test('renders provided header text', () => {
     const headerText = 'Testing';
 
-    const component = renderer.create(
+    const wrapper = shallow(
       <Header text={headerText} />
     );
 
-    const tree = component.toJSON();
-    const firstChild = tree.children[0];
-
-    expect(firstChild).toBe(headerText);
+    expect(wrapper.find('h2')).toHaveLength(1);
+    expect(wrapper.contains(headerText)).toBe(true);
   });
 });
