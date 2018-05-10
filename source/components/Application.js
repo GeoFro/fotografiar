05a78eb2fb331d79b7c6a1b50f3713b7621a286f
GeoFro <georgeharoldfrost@gmail.com>
2018-05-10 18:05:43 +1000

    Made Application component a function component

--- source/components/Application.js
+++ source/components/Application.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Stream from './Stream';
 import Collection from './Collection';
 
-class Application extends Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-md-4 text-center">
-            <Stream />
-          </div>
-          <div className="col-md-8">
-            <Collection />
-          </div>
-        </div>
+const Application = () =>(
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-md-4 text-center">
+        <Stream />
       </div>
-    );
-  }
-}
+      <div className="col-md-8">
+        <Collection />
+      </div>
+    </div>
+  </div>
+);
 
 export default Application;
